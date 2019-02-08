21669d1566b453cb14784e0ed59f924dfd2ca7b4
brumax27 <tit-max@live.fr>
2019-02-08 15:25:45 +0100

    fix MainActivity(2)

--- app/src/main/java/com/projet/android/jankenpon/MainActivity.java
+++ app/src/main/java/com/projet/android/jankenpon/MainActivity.java
@@ -1,10 +1,8 @@
 package com.projet.android.jankenpon;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 finish();
             }
-        }
+        });
         
         Button play = findViewById(R.id.play);
         play.setOnClickListener(new View.OnClickListener() {
