6a56ec368f71bbe865cca0d9902ab28496458092
brumax27 <tit-max@live.fr>
2019-02-09 17:10:27 +0100

    signIn OK

--- app/src/main/java/com/projet/android/jankenpon/activity/MainActivity.java
+++ app/src/main/java/com/projet/android/jankenpon/activity/MainActivity.java
@@ -1,22 +1,29 @@
 package com.projet.android.jankenpon.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.projet.android.jankenpon.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_CODE = 7645;
+    private GoogleSignInClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button result = findViewById(R.id.results);
-        result.setOnClickListener(new View.OnClickListener() {
+        getClient();
+
+        findViewById(R.id.results).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ScoresActivity.class);
@@ -24,22 +31,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button quit = findViewById(R.id.quit);
-        quit.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.quit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
         
-        Button play = findViewById(R.id.play);
-        play.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoadingGameActivity.class);
                 startActivity(intent);
             }
         });
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = client.getSignInIntent();
+                startActivityForResult(signInIntent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null){
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+        }
+    }
+
+    private void getClient(){
+        GoogleSignInOptions gso  = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        this.client = GoogleSignIn.getClient(this, gso);
     }
 }
 
