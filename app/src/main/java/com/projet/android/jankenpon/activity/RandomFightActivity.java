ee8ac00ce353ed2eb1a7bda559d4ebb3377db8f3
TuCao <ngtu.caong@gmail.com>
2019-02-23 11:16:43 +0100

    random fights activity updated

--- app/src/main/java/com/projet/android/jankenpon/activity/RandomFightActivity.java
+++ app/src/main/java/com/projet/android/jankenpon/activity/RandomFightActivity.java
@@ -1,182 +1,143 @@
 package com.projet.android.jankenpon.activity;
 
 import android.content.Intent;
-import android.os.Handler;
 import com.projet.android.jankenpon.R;
+
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
 public class RandomFightActivity extends AppCompatActivity {
+    Button b_rock, b_paper, b_scissors;
+    TextView tv_scores;
+    ImageView iv_PcChoice, iv_playerChoice, b_back, b_replay;
+    int playerScore, pcScore;
 
-        ImageView iv_player, iv_enemy;
-        TextView tv_points;
-        ProgressBar progressBar;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_fight);
 
-        Handler handler;
-        Runnable runnable;
-
-        Random r;
-
-        private final static int STATE_ROCK = 1;
-        private final static int STATE_PAPER = 2;
-        private final static int STATE_SCISSORS = 3;
-
-        int playerState = STATE_PAPER;
-        int enemyState = STATE_ROCK;
-
-        int currentTime = 5000;
-        int startTime = 5000;
-
-        int currentPoints = 0;
-
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_random_fight);
-
-            iv_player = findViewById(R.id.player);
-            iv_enemy = findViewById(R.id.enemy);
-            tv_points = findViewById(R.id.points);
-            progressBar = findViewById(R.id.progressBar);
-
-            findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(RandomFightActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            progressBar.setMax(startTime);
-            progressBar.setProgress(startTime);
-
-            tv_points.setText("Points: " + currentPoints);
-
-            handler = new Handler();
-            runnable = new Runnable() {
-                @Override
-                public void run() {
-                    currentTime = currentTime - 100;
-                    progressBar.setProgress(currentTime);
-
-                    r = new Random();
-                    enemyState = r.nextInt(3) + 1;
-
-                    if (currentTime > 0) {
-                        handler.postDelayed(runnable, 100);
-                    } else {
-                        iv_player.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                setPlayerImage(setPlayerPosition(playerState));
-                            }
-                        });
-
-                        setEnemyImage(enemyState);
-
-                        if ((playerState == STATE_ROCK && enemyState == STATE_SCISSORS) ||
-                            (playerState == STATE_PAPER && enemyState == STATE_ROCK) ||
-                            (playerState == STATE_SCISSORS && enemyState == STATE_PAPER))
-                            {
-                            currentPoints = currentPoints + 1;
-                            tv_points.setText("Points :" + currentPoints);
-
-                            startTime = startTime - 100;
-                            if (startTime < 1000) {
-                                startTime = 2000;
-                            }
-                            progressBar.setMax(startTime);
-                            currentTime = startTime;
-                            progressBar.setProgress(currentTime);
-
-                            //chay them game tiep theo khi win 1 luot
-                            handler.postDelayed(runnable, 100);
-                        } else {
-                            iv_player.setEnabled(false);
-                            Toast.makeText(RandomFightActivity.this, "Game Over!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-            };
-
-            handler.postDelayed(runnable, 100);
-        }
-
-        private void setEnemyImage (int state)
-        {
-            switch (state) {
-                case STATE_ROCK:
-                    iv_enemy.setImageResource(R.drawable.rock);
-                    enemyState = STATE_ROCK;
-                    break;
-                case STATE_PAPER:
-                    iv_enemy.setImageResource(R.drawable.paper);
-                    enemyState = STATE_PAPER;
-                    break;
-                case STATE_SCISSORS:
-                    iv_enemy.setImageResource(R.drawable.scissors);
-                    enemyState = STATE_SCISSORS;
-                    break;
+        b_rock = findViewById(R.id.b_rock);
+        b_paper = findViewById(R.id.b_paper);
+        b_scissors = findViewById(R.id.b_scissors);
+        b_back = findViewById(R.id.b_back);
+        b_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RandomFightActivity.this, MainActivity.class);
+                startActivity(intent);
             }
-        }
+        });
 
-        private void setRotation (final ImageView image, final int drawable ){
-            RotateAnimation rotateAnimation = new RotateAnimation(0, 90, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-            rotateAnimation.setDuration(100);
-            rotateAnimation.setInterpolator(new LinearInterpolator());
-            rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) {
-
-                }
-
-                @Override
-                public void onAnimationEnd(Animation animation) {
-                    image.setImageResource(drawable);
-                }
-
-                @Override
-                public void onAnimationRepeat(Animation animation) {
-
-                }
-            });
-            image.startAnimation(rotateAnimation);
-        }
-
-        private int setPlayerPosition (int position){
-            position = position + 1;
-            if (position == 4) {
-                position = 1;
+        b_replay = findViewById(R.id.b_replay);
+        b_replay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b_rock.setEnabled(true);
+                b_paper.setEnabled(true);
+                b_scissors.setEnabled(true);
+                tv_scores.setText("Computer : 0  Player : 0");
+                playerScore = 0;
+                pcScore = 0;
             }
-            return position;
+        });
+
+        iv_PcChoice = findViewById(R.id.iv_pc);
+        iv_playerChoice = findViewById(R.id.ic_player);
+
+        tv_scores = findViewById(R.id.tv_scores);
+        tv_scores.setText("Computer : 0  Player : 0");
+
+        b_rock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iv_playerChoice.setImageResource(R.drawable.rock);
+                play_turn ("rock");
+                tv_scores.setText("Computer : " + Integer.toString(pcScore) + "  Player : " + Integer.toString(playerScore));
+            }
+        });
+
+        b_paper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iv_playerChoice.setImageResource(R.drawable.paper);
+                play_turn ("paper");
+                tv_scores.setText("Computer : " + Integer.toString(pcScore) + "  Player : " + Integer.toString(playerScore));
+            }
+        });
+
+        b_scissors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iv_playerChoice.setImageResource(R.drawable.scissors);
+                play_turn ("scissors");
+                tv_scores.setText("Computer : " + Integer.toString(pcScore) + "  Player : " + Integer.toString(playerScore));
+            }
+        });
+    }
+
+    public void play_turn(String player_choice)
+    {
+        String pc_choice = "";
+        Random r = new Random();
+
+        // choose 1,2ou3
+        int pc_choice_number = r.nextInt(3)+1;
+        if (pc_choice_number == 1) {
+            pc_choice = "rock";
+        }
+        if (pc_choice_number == 2) {
+            pc_choice = "paper";
+        }
+        if (pc_choice_number == 3) {
+            pc_choice = "scissors";
         }
 
-        private void setPlayerImage (int state){
-            switch (state) {
-                case STATE_ROCK:
-                    setRotation(iv_player, R.drawable.rock);
-                    playerState = STATE_ROCK;
-                    break;
-                case STATE_PAPER:
-                    setRotation(iv_player, R.drawable.paper);
-                    playerState = STATE_PAPER;
-                    break;
-                case STATE_SCISSORS:
-                    setRotation(iv_player, R.drawable.scissors);
-                    playerState = STATE_SCISSORS;
-                    break;
+        //set the pc image based on his choice
+        if (pc_choice == "rock") {
+            iv_PcChoice.setImageResource(R.drawable.rock);
+        } else
+        if (pc_choice == "paper") {
+            iv_PcChoice.setImageResource(R.drawable.paper);
+        } else
+        if (pc_choice == "scissors") {
+            iv_PcChoice.setImageResource(R.drawable.scissors);
+        }
+
+        //pc player determine who win
+        if (playerScore == 10 || pcScore == 10) {
+            b_rock.setEnabled(false);
+            b_paper.setEnabled(false);
+            b_scissors.setEnabled(false);
+            if (playerScore == 10) {
+                Toast.makeText(RandomFightActivity.this, "YOU WIN !!!", Toast.LENGTH_SHORT).show();
+            } else if (pcScore == 10) {
+                Toast.makeText(RandomFightActivity.this, "YOU LOOSE !!!", Toast.LENGTH_SHORT).show();
             }
+        } else if (pc_choice == player_choice) {
+            Toast.makeText(RandomFightActivity.this, "Choose again please!", Toast.LENGTH_SHORT).show();
+        } else if (player_choice == "rock" && pc_choice == "scissors") {
+            playerScore++;
+        } else if (player_choice == "rock" && pc_choice == "paper") {
+            pcScore++;
+        } else if (player_choice == "scissors" && pc_choice == "rock") {
+            pcScore++;
+        } else if (player_choice == "scissors" && pc_choice == "paper") {
+            playerScore++;
+        } else if (player_choice == "paper" && pc_choice == "rock") {
+            playerScore++;
+        } else if (player_choice == "paper" && pc_choice == "scissors") {
+            pcScore++;
         }
     }
+}
 
 
